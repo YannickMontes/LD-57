9e390f57b53d73db18dc01cc301931422f2424e6
YannickMontes <montes.yannick@gmail.com>
2025-04-05 19:42:45 +0200

    TimeScale, obstacle & bonus

--- Scripts/player_bogger.gd
+++ Scripts/player_bogger.gd
@@ -1,32 +1,31 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const GRAVITY_WALL_MULTIPLIER = 40.0
-const MAX_DOWN_VELOCITY = 10000.0
-const LAUNCH_FORCE = 20000.0
+const GRAVITY_WALL_MULTIPLIER = 1.0
+const MAX_DOWN_VELOCITY = 1500.0
+const LAUNCH_FORCE = 10000.0
 
 @onready var launchFeedback = $LaunchFeedbackDir
+
+var currentSize := 10:
+	set(value):
+		currentSize = clamp(value, 0, 30)
 
 var isHoldingClick = false
 var beginHoldPosition = Vector2.ZERO
 var currentLaunchDirection = Vector2.ZERO
-var currentlyOnWall = false
+var isOnWall = false
+var affectedByGravity = false
 
 func _physics_process(delta: float) -> void:
 	
-	if is_on_wall():
-		if !currentlyOnWall:
-			velocity = Vector2.ZERO
+	if is_on_wall() && !isOnWall:
+		isOnWall = true
+		velocity = Vector2.ZERO
+		
+	if affectedByGravity:
 		velocity += get_gravity() * delta * GRAVITY_WALL_MULTIPLIER
 		if velocity.y >= MAX_DOWN_VELOCITY:
 			velocity.y = MAX_DOWN_VELOCITY
-		currentlyOnWall = true
-		print("on wall")
-	else:
-		currentlyOnWall = false
-		#print("NOT on wall")
 		
 	if Input.is_action_just_pressed("click"):
 		isHoldingClick = true
@@ -34,16 +33,26 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("click"):
 		isHoldingClick = false
 		velocity = currentLaunchDirection.normalized() * LAUNCH_FORCE
+		isOnWall = false
+		affectedByGravity = false
 		
-	var angleDeg = 0.0
 	if isHoldingClick:
 		var currentMousePos = get_viewport().get_mouse_position()
 		currentLaunchDirection = beginHoldPosition - currentMousePos
 		var angle = Vector2.UP.angle_to(currentLaunchDirection.normalized())
-		angleDeg = rad_to_deg(angle)
+		var angleDeg = rad_to_deg(angle)
 		launchFeedback.rotation_degrees = angleDeg
 		
 	launchFeedback.visible = isHoldingClick && currentLaunchDirection != Vector2.ZERO;
+	scale = Vector2.ONE * (currentSize / 10.0)
 
 	move_and_slide()
+
+func add_size(size):
+	currentSize += size
 	
+func on_obstacle_collide():
+	velocity = Vector2.ZERO
+	velocity.y = -450.0
+	affectedByGravity = true
+	TimeScaleManager.set_time_scale_for_duration(0.2, 0.8)
