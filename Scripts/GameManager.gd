84d351cd1a52d8b38076919154e61b6026cbb561
YannickMontes <montes.yannick@gmail.com>
2025-04-06 19:42:29 +0200

    Update GameManager.gd

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -7,15 +7,21 @@ var is_game_running: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	left_wall = get_tree().get_nodes_in_group("wall_left")[0]
-	right_wall = get_tree().get_nodes_in_group("wall_right")[0]
-
+	retrieve_walls()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not left_wall:
-		left_wall = get_tree().get_nodes_in_group("wall_left")[0]
-		right_wall = get_tree().get_nodes_in_group("wall_right")[0]
+		retrieve_walls()
 	pass
+
+func retrieve_walls():
+	var left_walls = get_tree().get_nodes_in_group("wall_left")
+	var right_walls = get_tree().get_nodes_in_group("wall_left")
+	if left_walls.size() > 0:
+		left_wall = left_walls[0]
+	if right_walls.size() > 0:
+		right_wall = right_walls[0]
 
 func game_over():
 	is_game_running = false
